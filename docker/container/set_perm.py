326c7523f9a96cdaa15cc0ce0fc20290a85a2626
Connor Kwon <connorkyg@gmail.com>
2023-02-20 15:38:30 +0900

    tmp

--- docker/container/set_perm.py
+++ docker/container/set_perm.py
@@ -1,13 +1,8 @@
 import os
-import subprocess
 from docker import cells
-
-
-def command(cmd):
-    return subprocess.run(cmd, shell=True)
-
 
 if __name__ == '__main__':
     local = '/SSR'
     for i in cells.Agent:
         os.chmod(f'{local}/{cells.Agent}', cells.Agent[i])
+    os.chown(f'{local}/*', 0, 0)
